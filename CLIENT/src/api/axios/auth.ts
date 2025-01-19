308d5260501668de131f8ed049043a641d2b7e8a
Fink2005 <phans3806@gmail.com>
2025-01-19 21:46:29 +0700

    update postman json

--- CLIENT/src/api/axios/auth.ts
+++ CLIENT/src/api/axios/auth.ts
@@ -4,7 +4,7 @@ import {
   signInTypeRespone,
   signUpTypeRespone,
   signUpTypeResquest,
-} from "../types/auth";
+} from "../../types/auth";
 
 const auth = {
   signIn: (payload: signInTypeRequest): Promise<signInTypeRespone> =>
